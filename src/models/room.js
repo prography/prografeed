76da623e8b0130c42e9946750509816ccea22644
Chancellor Kim <r54r45r54@gmail.com>
2017-11-02 02:02:26 +0900

    room 모델

---
+++ src/models/room.js
@@ -0,0 +1,36 @@
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+
+let roomSchema = new Schema({
+  ppt: {
+    type: String
+  },
+  owner: {
+    type: Schema.ObjectId,
+    ref: 'user'
+  },
+  conversations: [
+    { userId: Schema.ObjectId, nickname: String, body: String, date: Date }
+  ],
+  created_at: {type: Date, default: Date.now}
+})
+
+class Room {
+  static add (ppt, owner) {
+    return new Promise((resolve, reject) => {
+      new this({
+        ppt,
+        owner
+      }).save((err, result) => {
+        if (err) {
+          reject(new Error(err))
+        }
+        resolve(result)
+      })
+    })
+  }
+}
+
+roomSchema.loadClass(Room)
+
+module.exports = mongoose.model('room', roomSchema)
