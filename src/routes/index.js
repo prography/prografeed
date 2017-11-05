fd0704a43a71d97849d83153ae5dbc13607b50c7
JaeyunKang <kjy13411@gmail.com>
2017-11-05 09:12:16 +0000

    add upload feature

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 let express = require('express')
 let router = express.Router()
 let User = require('../models/user')
+let fileUpload = require('express-fileupload')
 const Room = require('../models/room')
+
+router.use(fileUpload())
 
 router.get('/', async (req, res) => {
   if (req.session.user) {
@@ -54,15 +57,42 @@ router.post('/', async (req, res, next) => {
 })
 
 // temp create room
-router.get('/new/:ppt', async (req, res, next) => {
-  const ppt = req.params.ppt
-  new Room({
-    ppt,
-    owner: req.session.user._id
-  }).save((err, result) => {
-    if (err) console.log(err)
-    res.send('good')
-  })
+router.post('/new/:ppt', async (req, res, next) => {
+  var ppt = req.files.ppt
+  var pptName = req.files.ppt.name
+  console.log(pptName)
+  var reg = /pptx/
+  if(!reg.test(pptName)) {
+    console.log('no match')
+    Room
+      .find()
+      .populate('owner', 'nickname')
+      .sort({'created_at': 1})
+      .exec((err, rooms) => {
+      if (err) console.log(err)
+      res.render('rooms', {rooms, nickname: req.session.user.nickname, errmsg: '파일이 pptx 형식이 아닙니다!'})
+    })
+  }
+  else {
+    ppt.mv(__dirname + '/../../public/' + pptName, function(err) {
+      if(err)
+        return res.status(500).send(err);
+    })
+    new Room({
+      ppt: pptName,
+      owner: req.session.user._id
+    }).save((err, result) => {
+      if (err) console.log(err)
+      Room
+      .find()
+      .populate('owner', 'nickname')
+      .sort({'created_at': 1})
+      .exec((err, rooms) => {
+      if (err) console.log(err)
+      res.render('rooms', {rooms, nickname: req.session.user.nickname})
+      })
+    })
+  }
 })
 
 module.exports = router
